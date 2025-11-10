f62ce60159878e5e5393c201847bb089a3a4298a
Morakz <63670601+GoHuFlame@users.noreply.github.com>
2025-11-10 17:27:42 -0600

    Update index.php

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Enviar WhatsApp</title>
-<link rel="stylesheet" href="estilos.css">
+<link rel="stylesheet" href="styles.css">
 </head>
 <body>
 
